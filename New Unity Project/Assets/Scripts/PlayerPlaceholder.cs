8bdc105dd6093d6b256a7d365832f270f96e7d1c
Ekin Nohutçu <60942915+ekinnohutcu@users.noreply.github.com>
2020-10-03 17:01:56 +0300

    player placeholder added

---
+++ New Unity Project/Assets/Scripts/PlayerPlaceholder.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPlaceholder : MonoBehaviour
+{
+    public GameObject player;
+
+    public GameObject planet;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, player.transform.position, 0.01f);
+
+        Vector3 gravDirection = (transform.position - planet.transform.position).normalized;
+        
+        Quaternion toRotation = Quaternion.FromToRotation(transform.up, gravDirection) * transform.rotation;
+        transform.rotation = Quaternion.Lerp(transform.rotation,toRotation, 0.01f);
+    }
+}
